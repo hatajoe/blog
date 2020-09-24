24d11ece18ef7b61a1480e1fb47338862df05c1f
Yusuke Hatanaka <arbalestimp@gmail.com>
2020-09-25 03:42:11 +0900

    add mysql to test action

--- go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/hatajoe/blog
-
-go 1.13
---
+++ src/ent/schema/article.go
@@ -0,0 +1,29 @@
+package schema
+
+import (
+	"time"
+
+	"github.com/facebook/ent"
+	"github.com/facebook/ent/schema/field"
+)
+
+// Article holds the schema definition for the Article entity.
+type Article struct {
+	ent.Schema
+}
+
+// Fields of the Article.
+func (Article) Fields() []ent.Field {
+	return []ent.Field{
+		field.Time("created_at").
+			Default(time.Now),
+		field.Time("updated_at").
+			Default(time.Now).
+			UpdateDefault(time.Now),
+	}
+}
+
+// Edges of the Article.
+func (Article) Edges() []ent.Edge {
+	return nil
+}
